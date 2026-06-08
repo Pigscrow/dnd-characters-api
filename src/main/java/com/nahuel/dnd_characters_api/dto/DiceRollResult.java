3ededccbadce16d2d0ed2a2816353938fc8e1491
Nahuel <nahuel0curti@gmail.com>
2026-06-08 17:20:27 -0300

    Fix credentials exposure, remove unused DTO, silence open-in-view warning

--- src/main/java/com/nahuel/dnd_characters_api/dto/DiceRollResult.java
+++
@@ -1,9 +0,0 @@
-package com.nahuel.dnd_characters_api.dto;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public record DiceRollResult(
-        @JsonProperty("result") int result,
-        @JsonProperty("details") String details,
-        @JsonProperty("input") String input
-) {}
